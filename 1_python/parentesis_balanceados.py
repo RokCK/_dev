377fad6f51595ea319b879a48c81d33438dc3a8d
Carlos Galindo <carlosxgalindo@gmail.com>
2021-12-30 06:03:59 -0600

    Agregando parentesis_balanceados.py

---
+++ 1_python/parentesis_balanceados.py
@@ -0,0 +1,19 @@
+def parentesis_balanceados(cadena):
+    apertura = "([{"
+    cierre = ")]}"
+    comparador = {")": "(", "]": "[", "}": "{"}
+    pila = []
+    for caracter in cadena:
+        if caracter in apertura:
+            pila.append(caracter)
+        elif caracter in cierre:
+            if len(caracter) == 0:
+                return False
+            if pila[-1] == comparador[caracter]:
+                pila.pop()
+            else:
+                return False
+    return len(pila) == 0
+
+print(parentesis_balanceados("{[()]}"))
+print(parentesis_balanceados("{[(]}"))
