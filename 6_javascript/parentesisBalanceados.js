1c9fcf4e5eb9ec56326a59fc112818ebb9da9ad3
Carlos Galindo <carlosxgalindo@gmail.com>
2021-12-30 22:23:23 -0600

    Agregando parentesisBalanceados.js

---
+++ 6_javascript/parentesisBalanceados.js
@@ -0,0 +1,25 @@
+function parentesisBalanceados(cadena) {
+  const apertura = "([{";
+  const cierre = ")]}";
+  const comparador = {")": "(", "]": "[", "}": "{"};
+  const pila = [];
+  for (const caracter of cadena) {
+    if (apertura.includes(caracter)) {
+      pila.push(caracter);
+    } else if (cierre.includes(caracter)) {
+      if (pila.length == 0) {
+	return false;
+      }
+      if (pila[pila.length -1] === comparador[caracter]) {
+	pila.pop();
+      } else {
+	return false;
+      }
+    }
+  }
+  return pila.length === 0;
+}
+exports.parentesisBalanceados = parentesisBalanceados;
+
+console.log(parentesisBalanceados("{[()]}"));
+console.log(parentesisBalanceados("{[(]}"));
